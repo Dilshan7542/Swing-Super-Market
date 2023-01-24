0e5b64dc06b4ded1ae886eef20259ec2e8150ea4
Dilshan7542 <dilshan.maduranga7542@gmail.com>
2023-01-24 07:46:53 +0530

    commit frist

---
+++ src/AppInitializer.java
@@ -0,0 +1,19 @@
+
+import addCustomer.AddCustomer;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Dilshan
+ */
+public class AppInitializer {
+    public static void main(String[] args) {
+        new AddCustomer().setVisible(true);
+    }
+            
+            
+}
